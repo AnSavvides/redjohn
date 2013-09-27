254ef311d8d29bead42d06ddab3b6b615eb5dc1e
Andreas Savvides <andreas@editd.com>
2013-09-27 19:57:17 +0100

    Adding debug

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 @app.route('/api/suspects/mentions', methods=['GET'])
 def get_mentions():
     return jsonify(results=get_suspect_mentions())
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
