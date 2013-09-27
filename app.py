db822a28686e4d69f1a51ece928a44542e3103d8
Andreas Savvides <andreas@editd.com>
2013-09-27 21:03:16 +0100

    Add jsonp support to flask

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import jsonify
 from red_api import get_suspect_mentions
+from jsonp_flask import support_jsonp
 
 app = Flask(__name__)
 
 @app.route('/api/suspects/mentions', methods=['GET'])
+@support_jsonp
 def get_mentions():
     return jsonify(results=get_suspect_mentions())
 
