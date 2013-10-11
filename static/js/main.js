c41187af0d65a20306b43294078d28db8829f7e0
Andreas Savvides <andreas@editd.com>
2013-10-11 14:57:15 +0100

    Show total tweets and suspect mentions

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,13 @@
     "use strict";
 
     function addScoresToSuspects(response) {
-        console.log(response);
+        _.each(response.results, function(mentions, suspect) {
+            $("#" + suspect).find(".mentions").text(mentions);
+        });
+    }
+
+    function addTotal(response) {
+        $("#tweet-count").text(response.results);
     }
 
     $.ajax({
@@ -11,6 +17,13 @@
         type: "GET",
         dataType: "jsonp",
         success: addScoresToSuspects
+    });
+
+    $.ajax({
+        url: "http://redjohn.herokuapp.com/api/tweets/count",
+        type: "GET",
+        dataType: "jsonp",
+        success: addTotal
     });
 
 })(jQuery);
