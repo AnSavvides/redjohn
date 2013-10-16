0517b7701b1352f6f7e887b3df9de3cd328c1d7c
Andreas Savvides <andreas@editd.com>
2013-10-16 23:13:34 +0100

    Use a template with handlebars instead of hard coding suspects

--- static/js/main.js
+++ static/js/main.js
@@ -2,19 +2,41 @@
 
     "use strict";
 
-    var apiUrl = "http://redjohn.herokuapp.com/api/tweets/";
+    var apiUrl = "http://redjohn.herokuapp.com/api/tweets/",
+        suspectNames = {
+            partridge: "Brett Partridge",
+            smith: "Reede Smith",
+            haffner: "Ray Haffner",
+            mcallister: "Sheriff McAllister",
+            kirkland: "Robert Kirkland",
+            bertram: "Gale Bertram",
+            stiles: "Brett Stiles"
+      };
 
     function getPrettyNumber(number) {
         return number.toLocaleString();
     }
 
     function addScoresToSuspects(response) {
-        var suspectCount = 0;
-
+        var suspectCount = 0,
+            suspects = [],
+            source = $("#suspect-template").html(),
+            template = Handlebars.compile(source),
+            $suspectsDiv = $("#suspects"),
+            context, $suspects;
+        
         _.each(response.results, function(mentions, suspect) {
-            $("#" + suspect).find(".mentions").text(getPrettyNumber(mentions) + " mentions");
+            suspects.push({
+                name: suspect,
+                prettyName: suspectNames[suspect],
+                mentions: getPrettyNumber(mentions)
+            });
             suspectCount += parseFloat(mentions);
         });
+
+        context = { suspects: suspects };
+        $suspects = template(context);
+        $suspectsDiv.html($suspects);
 
         $("#suspect-count").text(getPrettyNumber(suspectCount));
     }
