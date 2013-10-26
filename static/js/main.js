250c0825b1f6056b465ddfacf3645e9e37d9ab24
Andreas Savvides <andreas@editd.com>
2013-10-26 18:45:46 +0100

    Adapt to API changes to show popovers for suspects with latest tweets

--- static/js/main.js
+++ static/js/main.js
@@ -14,37 +14,70 @@
         },
         deadSuspects = ["partridge", "kirkland"];
 
+    // Adding commas to numbers, making them more readable
     function getPrettyNumber(number) {
         return number.toLocaleString("en-GB");
     }
 
-    function addScoresToSuspects(response) {
+    // Setting the suspect count
+    function setSuspectCount(count) {
+        $("#suspect-count").text(count);
+    }
+
+    // Sort the suspects from most mentioned to least mentioned
+    // and then render them accordingly.
+    function renderSuspects(response) {
         var suspectCount = 0,
             suspects = [],
             source = $("#suspect-template").html(),
             template = Handlebars.compile(source),
             $suspectsDiv = $("#suspects"),
-            sortedResults = _.sortBy(response.results, function(mentions) { return mentions; }),
+            sortedResults = _.sortBy(response.results, function(suspectInfo) { return suspectInfo.count; }),
             context, $suspects;
 
-        _.each(response.results, function(mentions, suspect) {
+        _.each(response.results, function(suspectInfo, suspect) {
+            var sortedResultsMatch = _.find(sortedResults, function(result) {
+                return result.count == suspectInfo.count;
+            });
+
             suspects.push({
                 name: suspect,
                 prettyName: suspectNames[suspect],
-                mentions: getPrettyNumber(mentions),
-                index: sortedResults.indexOf(mentions),
+                mentions: getPrettyNumber(suspectInfo.count),
+                index: sortedResults.indexOf(sortedResultsMatch),
                 dead: _.contains(deadSuspects, suspect)
             });
-            suspectCount += parseFloat(mentions);
+
+            suspectCount += parseFloat(suspectInfo.count);
         });
-        
+
+        // Ensure that these are sorted in descending order
         suspects = _.sortBy(suspects, "index").reverse();
 
         context = { suspects: suspects };
         $suspects = template(context);
         $suspectsDiv.html($suspects);
 
-        $("#suspect-count").text(getPrettyNumber(suspectCount));
+        setSuspectCount(getPrettyNumber(suspectCount));
+
+        // Now that the suspects have been rendered, let's assign
+        // popovers.
+        assignSuspectPopovers(response.results);
+    }
+
+    // Let's assign a popover to each suspect, to be triggered on hover,
+    // showing the latest tweets relevant to the suspect in question.
+    function assignSuspectPopovers(results) {
+        $(".mentions").each(function() {
+            var $suspectMentions = $(this),
+                suspectName = $suspectMentions.data("name");
+
+            $(this).popover({
+                title: suspectNames[suspectName],
+                content: results[suspectName].most_recent_tweets,
+                trigger: "hover"
+            });
+        });
     }
 
     function addTotal(response) {
@@ -55,7 +88,7 @@
         url: apiUrl + "suspects/count",
         type: "GET",
         dataType: "jsonp"
-    }).done(addScoresToSuspects);
+    }).done(renderSuspects);
 
     $.ajax({
         url: apiUrl + "count",
