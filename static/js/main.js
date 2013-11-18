eeed17d89ec592c9a30b01eb06ac042f5f9b4147
Andreas Savvides <andreas@editd.com>
2013-11-18 00:00:21 +0000

    Adding a tweets over time visualisation

--- static/js/main.js
+++ static/js/main.js
@@ -109,6 +109,53 @@
         $("#tweet-count").text(getPrettyNumber(response.results));
     }
 
+    function renderHistogram(response) {
+        var data = response.results,
+            margin = { top: 10, right: 5, bottom: 30, left: 50 },
+            width = 860 - margin.left - margin.right,
+            height = 200 - margin.top - margin.bottom,
+            barWidth = Math.ceil((width - 100) / data.length),
+            format = d3.time.format("%Y-%m-%d"),
+            firstDate = format.parse(data[0].date),
+            lastDate = format.parse($(data).get(-1).date),
+            x = d3.time.scale().domain([firstDate, lastDate]).range([0, width + margin.left + margin.right]),
+            y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.count; })]).range([height, 0]),
+            xAxis = d3.svg.axis().orient("bottom").scale(x).ticks(d3.time.week, 2),
+            yAxis = d3.svg.axis().orient("left").scale(y).ticks(4),
+            barChart;
+
+        // Creating an svg for our bar chart, ensuring that we have enough
+        // margin space for our axes and labels.
+        barChart = d3.select("#bar-chart")
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // Add bars to our chart
+        barChart.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("svg:rect")
+            .attr("x", function(datum, index) { return x(format.parse(datum.date)); })
+            .attr("y", function(datum) { return y(datum.count); })
+            .attr("height", function(datum) { return height - y(datum.count); })
+            .attr("width", barWidth)
+            .attr("fill", "#941100");
+
+        // Add an x axis
+        barChart.append("g")
+           .attr("class", "x axis")
+           .attr("transform", "translate(0," + height + ")")
+           .call(xAxis);
+
+        // Add a y axis
+        barChart.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+    }
+
     $.ajax({
         url: apiUrl + "suspects/count",
         type: "GET",
@@ -120,5 +167,11 @@
         type: "GET",
         dataType: "jsonp"
     }).done(addTotal);
+
+    $.ajax({
+        url: apiUrl + "time_series",
+        type: "GET",
+        dataType: "jsonp"
+    }).done(renderHistogram);
 
 })(jQuery);
